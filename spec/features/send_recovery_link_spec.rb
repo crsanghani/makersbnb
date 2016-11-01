78817e89ea0a135b595c211fe1e67c219b14cc2b
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-11-01 22:09:50 +0000

    small change made to reset password spec

--- spec/features/send_recovery_link_spec.rb
+++ spec/features/send_recovery_link_spec.rb
@@ -12,10 +12,10 @@ describe SendRecoverLink do
   end
 
  it "sends a message to mailgun when it is called" do
-   params = {from: "bookmarkmanager@mail.com",
+   params = {from: "hauntedbnb@mail.com",
              to: user.email,
              subject: "reset your password",
-             text: "click here to reset your password http://bookmarklandia.herokuapp.com/reset_password?token=#{user.password_token}" }
+             text: "click here to reset your password http://hauntedbnb.herokuapp.com/reset_password?token=#{user.password_token}" }
    expect(mail_gun_client).to receive(:send_message).with(sandbox_domain_name, params)
    described_class.call(user, mail_gun_client)
  end
