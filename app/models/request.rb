32e9c751d9740e8cf8280a4847f0d7f145cae830
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-11-02 16:32:17 +0000

    Requests model added

---
+++ app/models/request.rb
@@ -0,0 +1,33 @@
+class Request
+  include DataMapper::Resource
+
+  property :id, Serial
+
+  belongs_to :user
+  belongs_to :space
+
+  property :date_from, Date
+  property :date_to, Date
+
+  def request_dates
+    @dates_requested = (self.date_from..self.date_to).to_a
+    @dates_requested.each do |date|
+      if !self.space.dates_available.include?(date)
+        return Error "Dates not available. Check again, mortal!"
+      else
+        request_confirm_dates(@dates_requested, self.user_id, )
+      end
+    end
+  end
+
+  def request_confirm_dates
+
+  end
+
+  def confirm_dates
+    self.space.dates_available - @dates_requested
+  end
+
+
+
+end
